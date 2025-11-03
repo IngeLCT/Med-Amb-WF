0fe8eaf45e43d1a485a1bd1a8fb91422613ceeec
IngeLCT <ingenieria@lctdidacticos.com>
2025-11-03 11:13:47 -0600

    .

--- public/main.js
+++ public/main.js
@@ -1,16 +1,4 @@
 // main.js
-
-const firebaseConfig = {
-  apiKey: "AIzaSyAowEsndAOgwtEIfBABbq_GKNTX3bHh_VM",
-  authDomain: "calidadaire-677f9.firebaseapp.com",
-  databaseURL: "https://calidadaire-677f9-default-rtdb.firebaseio.com",
-  projectId: "calidadaire-677f9",
-  storageBucket: "calidadaire-677f9.firebasestorage.app",
-  messagingSenderId: "970353407925",
-  appId: "1:970353407925:web:6486958748783e422b8bfc"
-};
-
-firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
 // Inicializa la librería de alerta (usa el umbral definido dentro de stale-alert.js)
